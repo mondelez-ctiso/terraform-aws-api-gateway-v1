57efd5a8420bcea826dde7598ceaa3cf7121bfe5
Thanos Kataras <athanasios.kataras@mdlz.com>
2024-04-15 17:01:48 +0300

    feat(api-gateway-rest): fixing non-defined case

--- locals.tf
+++ locals.tf
@@ -46,7 +46,7 @@ locals {
   api_gateway_methods = [for method in var.api_gateway_methods :
     merge(method,
       {
-        key = "${method.resource_path}-${coalesce(try(method.api_method.http_method), "ANY")}"
+        key = "${method.resource_path}-${coalesce(try(method.api_method.http_method, null), "ANY")}"
       },
       { api_method = merge(
         var.api_gateway_method_default,
