82bb7cd4dac66b310fb3a75d1962bb139c11cac0
cdhesse <cdhesse76@gmail.com>
2024-05-20 03:19:26 -0400

    adding settings

--- main.tf
+++ main.tf
@@ -271,16 +271,16 @@ resource "aws_api_gateway_method_settings" "default" {
   dynamic "settings" {
     for_each = [each.value["api_method"]["settings"]]
     content {
-      metrics_enabled                            = lookup(settings.value, "metrics_enabled", false)
-      logging_level                              = lookup(settings.value, "logging_level", "ERROR")
-      caching_enabled                            = lookup(settings.value, "caching_enabled", false)
-      cache_data_encrypted                       = lookup(settings.value, "cache_data_encrypted", true)
-      cache_ttl_in_seconds                       = lookup(settings.value, "cache_ttl_in_seconds", null)
-      data_trace_enabled                         = lookup(settings.value, "data_trace_enabled", false)
-      require_authorization_for_cache_control    = lookup(settings.value, "require_authorization_for_cache_control", false)
-      throttling_burst_limit                     = lookup(settings.value, "throttling_burst_limit", -1)
-      throttling_rate_limit                      = lookup(settings.value, "throttling_rate_limit", -1)
-      unauthorized_cache_control_header_strategy = lookup(settings.value, "unauthorized_cache_control_header_strategy", null)
+      metrics_enabled = lookup(settings.value, "metrics_enabled", false)
+      logging_level   = lookup(settings.value, "logging_level", "ERROR")
+      # caching_enabled                            = lookup(settings.value, "caching_enabled", false)
+      # cache_data_encrypted                       = lookup(settings.value, "cache_data_encrypted", true)
+      # cache_ttl_in_seconds                       = lookup(settings.value, "cache_ttl_in_seconds", null)
+      # data_trace_enabled                         = lookup(settings.value, "data_trace_enabled", false)
+      # require_authorization_for_cache_control    = lookup(settings.value, "require_authorization_for_cache_control", false)
+      # throttling_burst_limit                     = lookup(settings.value, "throttling_burst_limit", -1)
+      # throttling_rate_limit                      = lookup(settings.value, "throttling_rate_limit", -1)
+      # unauthorized_cache_control_header_strategy = lookup(settings.value, "unauthorized_cache_control_header_strategy", null)
     }
   }
 }
